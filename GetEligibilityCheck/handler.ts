00f35f16bc4f2b216d11fe36ab8fccded5cc552f
Daniele Manni <dan.manni@gmail.com>
2020-05-28 15:19:34 +0200

    [#173017736] Add specs, refactory GetEligibilityCheck response

--- GetEligibilityCheck/handler.ts
+++ GetEligibilityCheck/handler.ts
@@ -1,6 +1,7 @@
 import { Context } from "@azure/functions";
 import * as df from "durable-functions";
 import * as express from "express";
+import { fromNullable } from "fp-ts/lib/Option";
 import { ContextMiddleware } from "io-functions-commons/dist/src/utils/middlewares/context_middleware";
 import { FiscalCodeMiddleware } from "io-functions-commons/dist/src/utils/middlewares/fiscalcode";
 import {
@@ -9,46 +10,73 @@ import {
 } from "io-functions-commons/dist/src/utils/request_middleware";
 import { readableReport } from "italia-ts-commons/lib/reporters";
 import {
-  IResponseErrorConflict,
   IResponseErrorInternal,
+  IResponseSuccessAccepted,
   IResponseSuccessJson,
-  ResponseErrorConflict,
   ResponseErrorInternal,
+  ResponseSuccessAccepted,
   ResponseSuccessJson
 } from "italia-ts-commons/lib/responses";
 import { FiscalCode } from "italia-ts-commons/lib/strings";
 import { ActivityResultSuccess } from "../EligibilityCheckActivity/handler";
-import { ConsultazioneSogliaIndicatoreResponse } from "../generated/definitions/ConsultazioneSogliaIndicatoreResponse";
+import { SottoSogliaEnum } from "../generated/definitions/ConsultazioneSogliaIndicatoreResponse";
+import { EligibilityCheck } from "../generated/definitions/EligibilityCheck";
+import { EligibilityCheckStatusEnum } from "../generated/definitions/EligibilityCheckStatus";
 import { initTelemetryClient } from "../utils/appinsights";
 
 type IGetEligibilityCheckHandler = (
   context: Context,
   fiscalCode: FiscalCode
 ) => Promise<
-  | IResponseSuccessJson<ConsultazioneSogliaIndicatoreResponse>
+  | IResponseSuccessJson<EligibilityCheck>
   | IResponseErrorInternal
-  | IResponseErrorConflict
+  | IResponseSuccessAccepted
 >;
 
 initTelemetryClient();
 
+// tslint:disable-next-line: cognitive-complexity
 export function GetEligibilityCheckHandler(): IGetEligibilityCheckHandler {
   return async (context, fiscalCode) => {
     const client = df.getClient(context);
     const status = await client.getStatus(fiscalCode);
-    if (status.runtimeStatus === df.OrchestrationRuntimeStatus.Running) {
-      return ResponseErrorConflict("Orchestrator already running");
-    }
-    return ActivityResultSuccess.decode(status.customStatus).fold<
-      | IResponseErrorInternal
-      | IResponseSuccessJson<ConsultazioneSogliaIndicatoreResponse>
-    >(
-      _ => {
-        context.log.error("GetEligibilityCheck|ERROR|%s", readableReport(_));
-        return ResponseErrorInternal("Invalid check status response");
-      },
-      _ => ResponseSuccessJson(_.data)
-    );
+    return ActivityResultSuccess.decode(status.customStatus)
+      .map(_ => {
+        const bonusValue = fromNullable(_.data.Componente)
+          .map(_1 =>
+            _1.length > 2
+              ? 50000
+              : _1.length === 2
+              ? 25000
+              : _1.length === 1
+              ? 15000
+              : 0
+          )
+          .getOrElse(0);
+        return EligibilityCheck.encode({
+          family_members: _.data.Componente || [],
+          max_amount: bonusValue,
+          max_tax_benefit: bonusValue, // TODO: Valorize properly this value
+          status:
+            _.data.SottoSoglia === SottoSogliaEnum.SI
+              ? EligibilityCheckStatusEnum.ELIGIBILE
+              : EligibilityCheckStatusEnum.INELIGIBLE
+        });
+      })
+      .fold<
+        | IResponseErrorInternal
+        | IResponseSuccessAccepted
+        | IResponseSuccessJson<EligibilityCheck>
+      >(
+        _ => {
+          if (status.runtimeStatus === df.OrchestrationRuntimeStatus.Running) {
+            return ResponseSuccessAccepted("Orchestrator already running");
+          }
+          context.log.error("GetEligibilityCheck|ERROR|%s", readableReport(_));
+          return ResponseErrorInternal("Invalid check status response");
+        },
+        _ => ResponseSuccessJson(_)
+      );
   };
 }
 
