af92282b368c5f7f125fcffb488e53491e0b2110
Daniele Manni <dan.manni@gmail.com>
2020-05-29 15:44:45 +0200

    [#173017736] rename INPS_PORT to INPS_SERVICE_PORT

--- EligibilityCheckActivity/index.ts
+++ EligibilityCheckActivity/index.ts
@@ -6,10 +6,10 @@ import { getEligibilityCheckActivityHandler } from "./handler";
 const INPS_SERVICE_HOST = getRequiredStringEnv("INPS_SERVICE_HOST");
 const INPS_SERVICE_ENDPOINT = getRequiredStringEnv("INPS_SERVICE_ENDPOINT");
 const INPS_SERVICE_PROTOCOL = getRequiredStringEnv("INPS_SERVICE_PROTOCOL");
-const INPS_PORT = getRequiredStringEnv("INPS_PORT");
+const INPS_SERVICE_PORT = getRequiredStringEnv("INPS_SERVICE_PORT");
 
 const soapClientAsync = createClient(
-  `${INPS_SERVICE_PROTOCOL}://${INPS_SERVICE_HOST}:${INPS_PORT}${INPS_SERVICE_ENDPOINT}` as NonEmptyString
+  `${INPS_SERVICE_PROTOCOL}://${INPS_SERVICE_HOST}:${INPS_SERVICE_PORT}${INPS_SERVICE_ENDPOINT}` as NonEmptyString
 );
 
 const eligibilityCheckActivityHandler = getEligibilityCheckActivityHandler(
