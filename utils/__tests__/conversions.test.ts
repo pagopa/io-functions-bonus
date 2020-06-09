33aff5eab7936d30db9333f3f09fb3e412768158
Emanuele De Cupis <emanuele@Emanueles-MBP.station>
2020-06-09 17:47:17 +0200

    implemented in the orchestrator

--- utils/__tests__/conversions.test.ts
+++ utils/__tests__/conversions.test.ts
@@ -5,6 +5,7 @@ import { EligibilityCheck as ApiEligibilityCheck } from "../../generated/definit
 
 import {
   toApiBonusActivation,
+  toApiBonusVacanzaBase,
   toApiEligibilityCheck,
   toModelBonusActivation,
   toModelEligibilityCheck
@@ -40,7 +41,9 @@ import {
 import { EligibilityCheck } from "../../generated/models/EligibilityCheck";
 
 import { IWithinRangeIntegerTag } from "italia-ts-commons/lib/numbers";
+import { readableReport } from "italia-ts-commons/lib/reporters";
 import { FiscalCode, NonEmptyString } from "italia-ts-commons/lib/strings";
+import { BonusVacanzaBase as ApiBonusVacanzaBase } from "../../generated/ade/BonusVacanzaBase";
 import { BonusActivationStatusEnum as ApiBonusActivationStatusEnum } from "../../generated/definitions/BonusActivationStatus";
 import { MaxBonusAmount } from "../../generated/definitions/MaxBonusAmount";
 import { MaxBonusTaxBenefit } from "../../generated/definitions/MaxBonusTaxBenefit";
@@ -334,5 +337,26 @@ describe("ApiBonusActivationFromModel", () => {
           expect(value).toEqual(domainObject);
         }
       );
+  });
+});
+
+describe("ApiBonusVacanzaBaseFromModel", () => {
+  it("should not decode an invalid domain object", () => {
+    const invalidDomainObject = { dsuRequest: { familyMembers: [] } };
+    // @ts-ignore needed to test an unrepresentable type assignment
+    const result = toApiBonusVacanzaBase(invalidDomainObject);
+    expect(isLeft(result)).toBeTruthy();
+  });
+
+  it("should decode domain", () => {
+    const domainObject = aBonusActivationDomainObject;
+    const result = toApiBonusVacanzaBase(domainObject);
+    if (isRight(result)) {
+      expect(ApiBonusVacanzaBase.is(result.value)).toBeTruthy();
+    } else {
+      fail(
+        `Valid domain object must be decoded: ${readableReport(result.value)}`
+      );
+    }
   });
 });
--- utils/conversions.ts
+++ utils/conversions.ts
@@ -2,12 +2,15 @@
  * Map API objects to domain objects
  */
 
-import { Either } from "fp-ts/lib/Either";
-import { Errors } from "io-ts";
+import { Either, left } from "fp-ts/lib/Either";
+import * as t from "io-ts";
+import { BonusVacanzaBase as ApiBonusVacanzaBase } from "../generated/ade/BonusVacanzaBase";
+import { NucleoFamiliareElem as ApiNucleoFamiliareElem } from "../generated/ade/NucleoFamiliareElem";
 import { BonusActivation as ApiBonusActivation } from "../generated/definitions/BonusActivation";
 import { EligibilityCheck as ApiEligibilityCheck } from "../generated/definitions/EligibilityCheck";
 import { BonusActivation } from "../generated/models/BonusActivation";
 import { EligibilityCheck } from "../generated/models/EligibilityCheck";
+import { FamilyMember } from "../generated/models/FamilyMember";
 import { renameObjectKeys } from "./rename_keys";
 import { camelCaseToSnakeCase, snakeCaseToCamelCase } from "./strings";
 
@@ -16,7 +19,7 @@ import { camelCaseToSnakeCase, snakeCaseToCamelCase } from "./strings";
  */
 export const toModelEligibilityCheck = (
   apiObj: ApiEligibilityCheck
-): Either<Errors, EligibilityCheck> => {
+): Either<t.Errors, EligibilityCheck> => {
   const camelCasedUntypedObj = renameObjectKeys(apiObj, k =>
     snakeCaseToCamelCase(k)
   );
@@ -28,7 +31,7 @@ export const toModelEligibilityCheck = (
  */
 export const toApiEligibilityCheck = (
   domainObj: EligibilityCheck
-): Either<Errors, ApiEligibilityCheck> => {
+): Either<t.Errors, ApiEligibilityCheck> => {
   const snakeCasedUntypedObj = renameObjectKeys(domainObj, k =>
     camelCaseToSnakeCase(k)
   );
@@ -40,7 +43,7 @@ export const toApiEligibilityCheck = (
  */
 export const toModelBonusActivation = (
   apiObj: ApiBonusActivation
-): Either<Errors, BonusActivation> => {
+): Either<t.Errors, BonusActivation> => {
   const camelCasedUntypedObj = renameObjectKeys(apiObj, k =>
     snakeCaseToCamelCase(k)
   );
@@ -52,9 +55,47 @@ export const toModelBonusActivation = (
  */
 export const toApiBonusActivation = (
   domainObj: BonusActivation
-): Either<Errors, ApiBonusActivation> => {
+): Either<t.Errors, ApiBonusActivation> => {
   const snakeCasedUntypedObj = renameObjectKeys(domainObj, k =>
     camelCaseToSnakeCase(k)
   );
   return ApiBonusActivation.decode(snakeCasedUntypedObj);
+};
+
+/**
+ * Maps FamilyMember domain object into NucleoFamiliareElem API object
+ */
+export const toApiNucleoFamiliareElem = (
+  familyMember: FamilyMember
+): Either<t.Errors, ApiNucleoFamiliareElem> => {
+  return ApiNucleoFamiliareElem.decode({
+    codiceFiscale: familyMember.fiscalCode
+  });
+};
+
+/**
+ * Maps BonusActivation domain object into an BonusVacanzaBase API object
+ */
+export const toApiBonusVacanzaBase = (
+  domainObject: BonusActivation
+): Either<t.Errors, ApiBonusVacanzaBase> => {
+  try {
+    const {
+      code,
+      applicantFiscalCode,
+      updatedAt,
+      dsuRequest: { maxAmount, familyMembers, hasDiscrepancies }
+    } = domainObject;
+    return ApiBonusVacanzaBase.decode({
+      codiceBuono: code,
+      codiceFiscaleDichiarante: applicantFiscalCode,
+      dataGenerazione: updatedAt.toISOString(),
+      flagDifformitaIsee: hasDiscrepancies ? 1 : 0,
+      importoMassimo: maxAmount,
+      nucleoFamiliare: familyMembers.map(toApiNucleoFamiliareElem)
+    });
+  } catch (err) {
+    // destructuring may fail
+    return left([{ message: err.message }] as t.Errors);
+  }
 };
