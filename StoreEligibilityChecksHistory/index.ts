8849309d7dc82ea4f8e9c553b2e7bfe039be723e
danilo spinelli <danilo.spinelli@pagopa.itt>
2020-06-10 23:53:21 +0200

    store eligibility check history

---
+++ StoreEligibilityChecksHistory/index.ts
@@ -0,0 +1,17 @@
+import { AzureFunction, Context } from "@azure/functions";
+
+const index: AzureFunction = async (
+  _: Context,
+  // tslint:disable-next-line: no-any
+  documents: readonly any[]
+) => {
+  return {
+    eligibilityChecksLogs: documents.map(d => ({
+      PartitionKey: `${d.id}-EC`,
+      Payload: JSON.stringify(d),
+      RowKey: d._ts
+    }))
+  };
+};
+
+export default index;
