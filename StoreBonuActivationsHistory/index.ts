a3eff4993ad3bf30febafe75b536ae5c4dee57c8
danilo spinelli <danilo.spinelli@pagopa.itt>
2020-06-10 23:30:08 +0200

    [#173168331] store bonus activations history

---
+++ StoreBonuActivationsHistory/index.ts
@@ -0,0 +1,17 @@
+import { AzureFunction, Context } from "@azure/functions";
+
+const index: AzureFunction = async (
+  _: Context,
+  // tslint:disable-next-line: no-any
+  documents: readonly any[]
+) => {
+  return {
+    bonusActivationsLogs: documents.map(d => ({
+      PartitionKey: `${d.id}-BA`,
+      Payload: JSON.stringify(d),
+      RowKey: d._ts
+    }))
+  };
+};
+
+export default index;
