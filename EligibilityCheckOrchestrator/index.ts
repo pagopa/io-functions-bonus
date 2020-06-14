889f2908364e569dfcc141159fa5b6c89d2a968c
Danilo Spinelli <gunzip@users.noreply.github.com>
2020-06-14 11:14:39 +0200

    Add eligibility check notifications (#56)

--- EligibilityCheckOrchestrator/index.ts
+++ EligibilityCheckOrchestrator/index.ts
@@ -6,16 +6,37 @@
 
 import { addSeconds } from "date-fns";
 import * as df from "durable-functions";
+import { none, Option, some } from "fp-ts/lib/Option";
+import { MessageContent } from "io-functions-commons/dist/generated/definitions/MessageContent";
 import { ActivityResult as DeleteEligibilityCheckActivityResult } from "../DeleteEligibilityCheckActivity/handler";
 import { ActivityResult } from "../EligibilityCheckActivity/handler";
-import { EligibilityCheckFailure } from "../generated/models/EligibilityCheckFailure";
-import { EligibilityCheckSuccessEligible } from "../generated/models/EligibilityCheckSuccessEligible";
-import { EligibilityCheckSuccessIneligible } from "../generated/models/EligibilityCheckSuccessIneligible";
+import { EligibilityCheck as ApiEligibilityCheck } from "../generated/definitions/EligibilityCheck";
+import { EligibilityCheckFailure } from "../generated/definitions/EligibilityCheckFailure";
+import { EligibilityCheckSuccessConflict } from "../generated/definitions/EligibilityCheckSuccessConflict";
+import { EligibilityCheckSuccessEligible } from "../generated/definitions/EligibilityCheckSuccessEligible";
+import { EligibilityCheckSuccessIneligible } from "../generated/definitions/EligibilityCheckSuccessIneligible";
 import { toEligibilityCheckFromDSU } from "../utils/conversions";
 import { MESSAGES } from "../utils/messages";
 import { retryOptions } from "../utils/retryPolicy";
 
 const NOTIFICATION_DELAY_SECONDS = 10;
+
+const getMessage = (
+  eligibilityCheck: ApiEligibilityCheck
+): Option<MessageContent> => {
+  return EligibilityCheckFailure.is(eligibilityCheck)
+    ? some(MESSAGES.EligibilityCheckFailure())
+    : EligibilityCheckSuccessEligible.is(eligibilityCheck) &&
+      eligibilityCheck.dsu_request.has_discrepancies
+    ? some(MESSAGES.EligibilityCheckSuccessEligibleWithDiscrepancies())
+    : EligibilityCheckSuccessEligible.is(eligibilityCheck)
+    ? some(MESSAGES.EligibilityCheckSuccessEligible())
+    : EligibilityCheckSuccessIneligible.is(eligibilityCheck)
+    ? some(MESSAGES.EligibilityCheckSuccessIneligible())
+    : EligibilityCheckSuccessConflict.is(eligibilityCheck)
+    ? some(MESSAGES.EligibilityCheckConflict())
+    : none;
+};
 
 const EligibilityCheckOrchestrator = df.orchestrator(function*(
   context: IOrchestrationFunctionContext
@@ -79,15 +100,8 @@ const EligibilityCheckOrchestrator = df.orchestrator(function*(
   );
 
   // send push notification with eligibility details
-  yield context.df.callActivity(
-    "SendMessageActivity",
-    EligibilityCheckFailure.is(eligibilityCheck)
-      ? MESSAGES.EligibilityCheckFailure()
-      : EligibilityCheckSuccessEligible.is(eligibilityCheck)
-      ? MESSAGES.EligibilityCheckSuccessEligible()
-      : EligibilityCheckSuccessIneligible.is(eligibilityCheck)
-      ? MESSAGES.EligibilityCheckSuccessIneligible()
-      : {}
+  getMessage(eligibilityCheck).map(
+    message => yield context.df.callActivity("SendMessageActivity", message)
   );
 
   return eligibilityCheckResponse;
--- utils/messages.ts
+++ utils/messages.ts
@@ -1,49 +1,74 @@
 // tslint:disable: no-duplicate-string object-literal-sort-keys
 
+import { MessageContent } from "io-functions-commons/dist/generated/definitions/MessageContent";
+
 export const MESSAGES = {
-  EligibilityCheckSuccessEligible: () => ({
-    subject: "Abbiamo completato le verifiche sul tuo ISEE",
-    markdown: `INPS ha completato le verifiche sull’ISEE del tuo nucleo familiare,
+  EligibilityCheckSuccessEligible: () =>
+    ({
+      subject: "Abbiamo completato le verifiche sul tuo ISEE",
+      markdown: `INPS ha completato le verifiche sull’ISEE del tuo nucleo familiare,
 e risulta che il tuo nucleo familiare ha diritto al Bonus Vacanze.  
 
 Clicca il pulsante qui sotto per proseguire con la richiesta del bonus.
 `
-  }),
+    } as MessageContent),
 
-  EligibilityCheckSuccessIneligible: () => ({
-    subject: "Abbiamo completato le verifiche sul tuo ISEE",
-    markdown: `INPS ha completato le verifiche e risulta che l’ISEE del tuo nucleo familiare supera la soglia di 40.000€.
+  EligibilityCheckSuccessIneligible: () =>
+    ({
+      subject: "Abbiamo completato le verifiche sul tuo ISEE",
+      markdown: `INPS ha completato le verifiche e risulta che l’ISEE del tuo nucleo familiare supera la soglia di 40.000€.
     Di conseguenza non potete accedere al Bonus Vacanze.  
 
 Se non ti risulta corretto, oppure se il tuo reddito familiare è cambiato dall’ultimo ISEE creato,
 puoi aggiornarlo effettuando una nuova Dichiarazione Sostituiva Unica (DSU) sul sito di INPS.
 `
-  }),
+    } as MessageContent),
 
-  EligibilityCheckFailure: () => ({
-    subject: "Abbiamo completato le verifiche sul tuo ISEE",
-    markdown: `INPS ha completato le verifiche e risulta che il tuo nucleo familiare non ha un ISEE valido.  
+  EligibilityCheckFailure: () =>
+    ({
+      subject: "Abbiamo completato le verifiche sul tuo ISEE",
+      markdown: `INPS ha completato le verifiche e risulta che il tuo nucleo familiare non ha un ISEE valido.  
 
 E’ necessario presentare una Dichiarazione Sostitutiva Unica (DSU) per il calcolo dell’ISEE per ottenere il Bonus Vacanze.  
 Se vuoi, puoi fare subito una simulazione o la richiesta online sul sito di INPS.
 `
-  }),
+    } as MessageContent),
 
-  EligibilityCheckSuccessEligibleWithDiscrepancies: () => ({
-    subject: "Abbiamo completato le verifiche sul tuo ISEE",
-    markdown: `INPS ha completato le verifiche sull’ISEE del tuo nucleo familiare, e risulta che il tuo nucleo familiare ha diritto al Bonus Vacanze.  
+  EligibilityCheckConflict: () =>
+    ({
+      subject: "Abbiamo completato le verifiche sul tuo ISEE",
+      markdown: `INPS ha completato le verifiche sull’ISEE del tuo nucleo familiare, e sembra che qualcuno del tuo nucleo familiare abbia già richiesto il Bonus Vacanze.  
+
+Se la richiesta è andata a buon fine, puoi trovare il Bonus Vacanze nella sezione Pagamenti.
+`
+    } as MessageContent),
+
+  EligibilityCheckSuccessEligibleWithDiscrepancies: () =>
+    ({
+      subject: "Abbiamo completato le verifiche sul tuo ISEE",
+      markdown: `INPS ha completato le verifiche sull’ISEE del tuo nucleo familiare, e risulta che il tuo nucleo familiare ha diritto al Bonus Vacanze.  
 
 Ti avvisiamo, però, che INPS ha riscontrato alcune omissioni o difformità nel tuo ISEE, per cui ora puoi continuare con la richiesta del bonus,
 ma potrai essere chiamato in futuro a colmare eventuali lacune nella documeazione presentata.
 
 Clicca il pulsante qui sotto per proseguire con la richiesta del bonus.
 `
-  }),
+    } as MessageContent),
 
-  BonusActivationSuccess: () => ({
-    subject: "Il tuo Bonus Vacanze è attivo!",
-    markdown: `"Buone notizie! Il Bonus Vacanze per il tuo nucleo familiare è attivo e lo potrai trovare all’interno della sezione pagamenti.  
+  BonusActivationSuccess: () =>
+    ({
+      subject: "Il tuo Bonus Vacanze è attivo!",
+      markdown: `Buone notizie! Il Bonus Vacanze per il tuo nucleo familiare è attivo e lo potrai trovare all’interno della sezione pagamenti.  
 
 Ti ricordiamo che chiunque della tua famiglia potrà spenderlo presso le strutture ricettive aderenti dal 1 luglio al 31 dicembre 2020.`
-  })
+    } as MessageContent),
+
+  BonusActivationFailure: () =>
+    ({
+      subject: "Abbiamo riscontrato dei problemi",
+      markdown: `Si è verificato un errore nel processare la tua richiesta di Bonus.  
+
+Dovrai iniziare una nuova domanda.
+Clicca il pulsante qui sotto per procedere.`
+    } as MessageContent)
 };
