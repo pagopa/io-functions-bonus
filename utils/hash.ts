80ffdec8c9ff0621583facc3f69f26566434c8ad
Emanuele De Cupis <emanuele@Emanueles-MBP.station>
2020-06-15 09:42:58 +0200

    pr review

--- utils/hash.ts
+++ utils/hash.ts
@@ -1,11 +1,12 @@
 import * as crypto from "crypto";
+import { NonEmptyString } from "italia-ts-commons/lib/strings";
 import { FamilyMembers } from "../generated/models/FamilyMembers";
 import { FamilyUID } from "../generated/models/FamilyUID";
 
-export const toHash = (s: string): string => {
+export const toHash = (s: string): NonEmptyString => {
   const hash = crypto.createHash("sha256");
   hash.update(s);
-  return hash.digest("hex");
+  return hash.digest("hex") as NonEmptyString;
 };
 
 export const generateFamilyUID = (familyMembers: FamilyMembers): FamilyUID =>
