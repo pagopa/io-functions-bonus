5d86fc167abfcf54ca7f882974bf245e7f5bf70e
Emanuele De Cupis <emanuele@Emanueles-MBP.station>
2020-06-15 10:11:45 +0200

    fix

--- utils/conversions.ts
+++ utils/conversions.ts
@@ -90,7 +90,7 @@ export const toModelBonusActivation = (
   );
   return BonusActivation.decode(camelCasedUntypedObj).chain(base =>
     BonusActivationWithFamilyUID.decode({
-      ...camelCasedUntypedObj,
+      ...base,
       familyUID: generateFamilyUID(base.dsuRequest.familyMembers)
     })
   );
@@ -102,10 +102,10 @@ export const toModelBonusActivation = (
 export const toApiBonusActivation = (
   domainObj: BonusActivationWithFamilyUID
 ): Either<t.Errors, ApiBonusActivation> => {
-  const {
-    family_members,
-    ...snakeCasedUntypedObj
-  } = renameObjectKeys(domainObj, k => camelCaseToSnakeCase(k));
+  const { familyUID, ...cleanDomainObject } = domainObj;
+  const snakeCasedUntypedObj = renameObjectKeys(cleanDomainObject, k =>
+    camelCaseToSnakeCase(k)
+  );
   return ApiBonusActivation.decode(snakeCasedUntypedObj);
 };
 
