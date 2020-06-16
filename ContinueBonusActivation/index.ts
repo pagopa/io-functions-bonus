62768e6e3e9a1412c7c1bbb20430cf978805cef5
danilo spinelli <danilo.spinelli@pagopa.itt>
2020-06-16 21:17:07 +0200

    continue bonus activation handler

--- ContinueBonusActivation/index.ts
+++ ContinueBonusActivation/index.ts
@@ -1,29 +1,9 @@
 import { AzureFunction, Context } from "@azure/functions";
 import * as express from "express";
-import * as documentDbUtils from "io-functions-commons/dist/src/utils/documentdb";
-import { getRequiredStringEnv } from "io-functions-commons/dist/src/utils/env";
 import { secureExpressApp } from "io-functions-commons/dist/src/utils/express";
 import { setAppContext } from "io-functions-commons/dist/src/utils/middlewares/context_middleware";
 import createAzureFunctionHandler from "io-functions-express/dist/src/createAzureFunctionsHandler";
-import {
-  BONUS_ACTIVATION_COLLECTION_NAME,
-  BonusActivationModel
-} from "../models/bonus_activation";
-import { documentClient } from "../utils/cosmosdb";
-import { GetBonusActivation } from "./handler";
-
-const cosmosDbName = getRequiredStringEnv("COSMOSDB_BONUS_DATABASE_NAME");
-
-const documentDbDatabaseUrl = documentDbUtils.getDatabaseUri(cosmosDbName);
-const bonusActivationCollectionUrl = documentDbUtils.getCollectionUri(
-  documentDbDatabaseUrl,
-  BONUS_ACTIVATION_COLLECTION_NAME
-);
-
-const bonusActivationModel = new BonusActivationModel(
-  documentClient,
-  bonusActivationCollectionUrl
-);
+import { ContinueBonusActivation } from "./handler";
 
 // Setup Express
 const app = express();
@@ -31,8 +11,8 @@ secureExpressApp(app);
 
 // Add express route
 app.get(
-  "/api/v1/bonus/vacanze/activations/:fiscalcode/:bonus_id",
-  GetBonusActivation(bonusActivationModel)
+  "/api/v1/bonus/vacanze/activations/:fiscalcode",
+  ContinueBonusActivation()
 );
 
 const azureFunctionHandler = createAzureFunctionHandler(app);
