5fa776fe61d8d90a4c553c7678526c2852357b80
danilo spinelli <danilo.spinelli@pagopa.itt>
2020-06-22 00:36:33 +0200

    log ade request responses

--- SendBonusActivationActivity/index.ts
+++ SendBonusActivationActivity/index.ts
@@ -1,3 +1,4 @@
+import { constVoid } from "fp-ts/lib/function";
 import { getRequiredStringEnv } from "io-functions-commons/dist/src/utils/env";
 import { agent } from "italia-ts-commons";
 import {
@@ -6,8 +7,10 @@ import {
   toFetch
 } from "italia-ts-commons/lib/fetch";
 import { IntegerFromString } from "italia-ts-commons/lib/numbers";
+import { NonEmptyString } from "italia-ts-commons/lib/strings";
 import { Millisecond } from "italia-ts-commons/lib/units";
 import { UrlFromString } from "italia-ts-commons/lib/url";
+import { traceAdeRequest } from "../services/loggers";
 import { ADEClient } from "../utils/adeClient";
 import SendBonusActivationHandler from "./handler";
 
@@ -38,7 +41,26 @@ const fetchWithTimeout = setFetchTimeout(
 
 const httpFetch = toFetch(fetchWithTimeout);
 
-const adeClient = ADEClient(adeServiceEndpoint, httpFetch);
+const logHttpFetch = (f: typeof fetch): typeof fetch => {
+  return (input, init) => {
+    const res = f(input, init);
+    res
+      .then(_ => _.clone())
+      .then(_ => _.json())
+      .then(_ =>
+        traceAdeRequest({
+          Id: _?.result?.codiceBuono || new Date().getTime().toString(),
+          RequestPayload: init?.body?.toString() || "",
+          ResponsePayload: _,
+          Timestamp: new Date()
+        })
+      )
+      .catch(constVoid);
+    return res;
+  };
+};
+
+const adeClient = ADEClient(adeServiceEndpoint, logHttpFetch(httpFetch));
 
 const SendBonusActivationActivity = SendBonusActivationHandler(adeClient);
 
