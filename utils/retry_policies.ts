adf0e98b6bc0abc74423c70a4fa2ff764e641cc3
danilo spinelli <danilo.spinelli@pagopa.itt>
2020-06-26 10:08:00 +0200

    make policies configurable

--- utils/retry_policies.ts
+++ utils/retry_policies.ts
@@ -1,8 +1,17 @@
 import { RetryOptions } from "durable-functions";
 import { IntegerFromString } from "italia-ts-commons/lib/numbers";
 
-const RETRY_OPTIONS_FIRST_RETRY_INTERVAL_MS = 500;
+const RETRY_OPTIONS_FIRST_RETRY_INTERVAL_MS = IntegerFromString.decode(
+  process.env.RETRY_OPTIONS_FIRST_RETRY_INTERVAL_MS
+).getOrElse(500);
 
+const RETRY_OPTIONS_BACKOFF_COEFFICIENT = IntegerFromString.decode(
+  process.env.RETRY_OPTIONS_BACKOFF_COEFFICIENT
+).getOrElse(1.5);
+
+//
+// Used for internal calls between IO services
+//
 const INTERNAL_RETRY_OPTIONS_MAX_ATTEMPTS = IntegerFromString.decode(
   process.env.INTERNAL_RETRY_OPTIONS_MAX_ATTEMPTS
 ).getOrElse(10);
@@ -11,8 +20,11 @@ export const internalRetryOptions: RetryOptions = new RetryOptions(
   INTERNAL_RETRY_OPTIONS_MAX_ATTEMPTS
 );
 // tslint:disable-next-line: no-object-mutation
-internalRetryOptions.backoffCoefficient = 1.5;
+internalRetryOptions.backoffCoefficient = RETRY_OPTIONS_BACKOFF_COEFFICIENT;
 
+//
+// Used for external calls (INPS / ADE) services
+//
 const EXTERNAL_RETRY_OPTIONS_MAX_ATTEMPTS = IntegerFromString.decode(
   process.env.EXTERNAL_RETRY_OPTIONS_MAX_ATTEMPTS
 ).getOrElse(20);
@@ -21,4 +33,4 @@ export const externalRetryOptions: RetryOptions = new RetryOptions(
   EXTERNAL_RETRY_OPTIONS_MAX_ATTEMPTS
 );
 // tslint:disable-next-line: no-object-mutation
-externalRetryOptions.backoffCoefficient = 1.5;
+externalRetryOptions.backoffCoefficient = RETRY_OPTIONS_BACKOFF_COEFFICIENT;
