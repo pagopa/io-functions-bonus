07252e39a4669facc1c5071337b77ebf19471b93
danilo spinelli <danilo.spinelli@pagopa.itt>
2020-07-15 14:56:23 +0200

    Revert "[#173810181] Remove cosmosdb bindings from EligibilityCheck (#145)"
    This reverts commit 18532195076f7880a18494e578080f4a218d2db4.

--- EligibilityCheck/__tests__/handler.test.ts
+++ EligibilityCheck/__tests__/handler.test.ts
@@ -1,5 +1,3 @@
-import { right } from "fp-ts/lib/Either";
-import { none, some } from "fp-ts/lib/Option";
 import { FiscalCode } from "italia-ts-commons/lib/strings";
 import {
   context,
@@ -8,15 +6,8 @@ import {
   mockStatusCompleted,
   mockStatusRunning
 } from "../../__mocks__/durable-functions";
-import {
-  aBonusId,
-  aEligibilityCheckSuccessEligibleValid
-} from "../../__mocks__/mocks";
-import {
-  BonusProcessing,
-  BonusProcessingModel
-} from "../../models/bonus_processing";
-import { EligibilityCheckModel } from "../../models/eligibility_check";
+import { aBonusId } from "../../__mocks__/mocks";
+import { BonusProcessing } from "../../models/bonus_processing";
 import { makeStartEligibilityCheckOrchestratorId } from "../../utils/orchestrators";
 import { EligibilityCheckHandler } from "../handler";
 
@@ -29,18 +20,6 @@ const simulateOrchestratorIsRunning = (forOrchestratorId: string) => {
   );
 };
 const aFiscalCode = "AAABBB80A01C123D" as FiscalCode;
-
-const mockEligibilityCheckFind = jest
-  .fn()
-  .mockImplementation(() => Promise.resolve(right(none)));
-const mockEligibilityCheckModel = ({
-  find: mockEligibilityCheckFind
-} as unknown) as EligibilityCheckModel;
-
-const mockBonusProcessingFind = jest.fn();
-const mockBonusProcessingModel = ({
-  find: mockBonusProcessingFind
-} as unknown) as BonusProcessingModel;
 
 describe("EligibilityCheckHandler", () => {
   beforeEach(() => {
@@ -55,14 +34,13 @@ describe("EligibilityCheckHandler", () => {
   });
 
   it("should returns a 403 status response if a bonus activation is running", async () => {
-    mockBonusProcessingFind.mockImplementation(() =>
-      Promise.resolve(right(some(1)))
-    );
+    // tslint:disable-next-line: no-object-mutation
+    context.bindings.processingBonusIdIn = BonusProcessing.encode({
+      bonusId: aBonusId,
+      id: aFiscalCode
+    });
 
-    const handler = EligibilityCheckHandler(
-      mockEligibilityCheckModel,
-      mockBonusProcessingModel
-    );
+    const handler = EligibilityCheckHandler();
 
     const response = await handler(context, aFiscalCode);
 
@@ -72,50 +50,19 @@ describe("EligibilityCheckHandler", () => {
     simulateOrchestratorIsRunning(
       makeStartEligibilityCheckOrchestratorId(aFiscalCode)
     );
-    mockBonusProcessingFind.mockImplementation(() =>
-      Promise.resolve(right(none))
-    );
 
-    const handler = EligibilityCheckHandler(
-      mockEligibilityCheckModel,
-      mockBonusProcessingModel
-    );
+    const handler = EligibilityCheckHandler();
 
     const response = await handler(context, aFiscalCode);
 
     expect(response.kind).toBe("IResponseSuccessAccepted");
   });
-  it("should returns ResponseSuccessRedirectToResource if exists a Valid EligibilityCheck", async () => {
-    mockEligibilityCheckFind.mockImplementationOnce(() =>
-      Promise.resolve(right(some(aEligibilityCheckSuccessEligibleValid)))
-    );
-    mockBonusProcessingFind.mockImplementation(() =>
-      Promise.resolve(right(none))
-    );
-
-    const handler = EligibilityCheckHandler(
-      mockEligibilityCheckModel,
-      mockBonusProcessingModel
-    );
-
-    const response = await handler(context, aFiscalCode);
-
-    expect(mockStartNew).toBeCalledTimes(0);
-
-    expect(response.kind).toBe("IResponseSuccessRedirectToResource");
-  });
   it("should returns ResponseSuccessRedirectToResource if EligibilityCheckOrchestrator starts successfully", async () => {
     mockStartNew.mockImplementationOnce(async (_, __, ___) => {
       return "instanceId";
     });
-    mockBonusProcessingFind.mockImplementation(() =>
-      Promise.resolve(right(none))
-    );
 
-    const handler = EligibilityCheckHandler(
-      mockEligibilityCheckModel,
-      mockBonusProcessingModel
-    );
+    const handler = EligibilityCheckHandler();
 
     const response = await handler(context, aFiscalCode);
 
@@ -131,14 +78,8 @@ describe("EligibilityCheckHandler", () => {
     mockStartNew.mockImplementationOnce(async (_, __, ___) => {
       throw new Error("Error starting the orchestrator");
     });
-    mockBonusProcessingFind.mockImplementation(() =>
-      Promise.resolve(right(none))
-    );
 
-    const handler = EligibilityCheckHandler(
-      mockEligibilityCheckModel,
-      mockBonusProcessingModel
-    );
+    const handler = EligibilityCheckHandler();
 
     const response = await handler(context, aFiscalCode);
 
