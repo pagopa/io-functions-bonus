95d43ab9533605d4502fcdfe81adcac9b689f483
Daniele Manni <dan.manni@gmail.com>
2020-07-31 15:47:01 +0200

    GetAllBonusActivation tests

--- GetBonusActivation/__tests__/handler.test.ts
+++ GetBonusActivation/__tests__/handler.test.ts
@@ -1,34 +1,21 @@
-import { left, right } from "fp-ts/lib/Either";
 import { none, some } from "fp-ts/lib/Option";
+import { fromLeft, taskEither } from "fp-ts/lib/TaskEither";
+import { CosmosErrorResponse } from "io-functions-commons/dist/src/utils/cosmosdb_model";
 import { response as MockResponse } from "jest-mock-express";
 import { context } from "../../__mocks__/durable-functions";
 import {
   aBonusId,
   aFiscalCode,
-  aRetrievedBonusActivation,
-  aRetrievedBonusProcessing
+  aRetrievedBonusActivation
 } from "../../__mocks__/mocks";
 import { BonusActivationModel } from "../../models/bonus_activation";
-import { BonusProcessingModel } from "../../models/bonus_processing";
 import { GetBonusActivationHandler } from "../handler";
-
-const mockBonusProcessingCreate = jest.fn().mockImplementation(async _ => {
-  return right(aRetrievedBonusProcessing);
-});
-const mockBonusProcessingFind = jest.fn().mockImplementation(async () =>
-  // happy path: retrieve a valid eligible check
-  right(none)
-);
-const mockBonusProcessingModel = ({
-  create: mockBonusProcessingCreate,
-  find: mockBonusProcessingFind
-} as unknown) as BonusProcessingModel;
 
 // mockBonusActivationModel
 const mockBonusActivationFindBonusActivationForUser = jest
   .fn()
-  .mockImplementation(async _ => {
-    return right(some(aRetrievedBonusActivation));
+  .mockImplementation(_ => {
+    return taskEither.of(some(aRetrievedBonusActivation));
   });
 const mockBonusActivationModel = ({
   findBonusActivationForUser: mockBonusActivationFindBonusActivationForUser
@@ -46,25 +33,9 @@ describe("GetBonusActivationHandler", () => {
     expect(response.status).toHaveBeenCalledWith(200);
   });
 
-  it("should fail if the find query throws", async () => {
-    mockBonusActivationFindBonusActivationForUser.mockImplementationOnce(
-      async () => {
-        throw new Error("any error");
-      }
-    );
-    const handler = GetBonusActivationHandler(mockBonusActivationModel);
-
-    const result = await handler(context, aFiscalCode, aBonusId);
-
-    expect(result.kind).toBe("IResponseErrorInternal");
-    const response = MockResponse();
-    result.apply(response);
-    expect(response.status).toHaveBeenCalledWith(500);
-  });
-
   it("should fail if the find query fails", async () => {
-    mockBonusActivationFindBonusActivationForUser.mockImplementationOnce(
-      async () => left(new Error("any error"))
+    mockBonusActivationFindBonusActivationForUser.mockImplementationOnce(() =>
+      fromLeft(CosmosErrorResponse(new Error("any error")))
     );
     const handler = GetBonusActivationHandler(mockBonusActivationModel);
 
@@ -77,8 +48,8 @@ describe("GetBonusActivationHandler", () => {
   });
 
   it("should notify the user if there is no bonus activation fot the current user", async () => {
-    mockBonusActivationFindBonusActivationForUser.mockImplementationOnce(
-      async () => right(none)
+    mockBonusActivationFindBonusActivationForUser.mockImplementationOnce(() =>
+      taskEither.of(none)
     );
     const handler = GetBonusActivationHandler(mockBonusActivationModel);
 
