caa1758b6d7af032fa8ed28c990188ce7b6638ab
Daniele Manni <dan.manni@gmail.com>
2020-08-10 16:04:10 +0200

    [#174034404] upgrade fixtures

--- docker/fixtures/index.ts
+++ docker/fixtures/index.ts
@@ -1,11 +1,7 @@
 /**
  * Insert fake data into CosmosDB database emulator.
  */
-import {
-  CollectionMeta,
-  DocumentClient as DocumentDBClient,
-  UriFactory
-} from "documentdb";
+import { ContainerResponse } from "@azure/cosmos";
 import { Either, left, right } from "fp-ts/lib/Either";
 import {
   Profile,
@@ -17,55 +13,37 @@ import {
   SERVICE_COLLECTION_NAME,
   ServiceModel
 } from "io-functions-commons/dist/src/models/service";
-import * as documentDbUtils from "io-functions-commons/dist/src/utils/documentdb";
 import { getRequiredStringEnv } from "io-functions-commons/dist/src/utils/env";
-import { documentClient } from "../../services/cosmosdb";
+import { cosmosClient } from "../../services/cosmosdb";
 
 const cosmosDbName = getRequiredStringEnv("COSMOSDB_BONUS_DATABASE_NAME");
 
-const documentDbDatabaseUrl = documentDbUtils.getDatabaseUri(cosmosDbName);
+const databaseClient = cosmosClient.database(cosmosDbName);
 
 function createDatabase(databaseName: string): Promise<Either<Error, void>> {
-  return new Promise(resolve => {
-    documentClient.createDatabase({ id: databaseName }, (err, _) => {
-      if (err) {
-        return resolve(left<Error, void>(new Error(err.body)));
-      }
-      resolve(right<Error, void>(void 0));
-    });
-  });
+  return cosmosClient.databases
+    .createIfNotExists({ id: databaseName })
+    .then(_ => right<Error, void>(void 0))
+    .catch(err => left<Error, void>(new Error(err)));
 }
 
 function createCollection(
   collectionName: string,
   partitionKey: string
-): Promise<Either<Error, CollectionMeta>> {
-  return new Promise(resolve => {
-    const dbUri = UriFactory.createDatabaseUri(cosmosDbName);
-    documentClient.createCollection(
-      dbUri,
-      {
-        id: collectionName,
-        partitionKey: {
-          kind: "Hash",
-          paths: [`/${partitionKey}`]
-        }
-      },
-      (err, ret) => {
-        if (err) {
-          return resolve(left<Error, CollectionMeta>(new Error(err.body)));
-        }
-        resolve(right<Error, CollectionMeta>(ret));
-      }
-    );
-  });
+): Promise<Either<Error, ContainerResponse>> {
+  return databaseClient.containers
+    .createIfNotExists({
+      id: collectionName,
+      partitionKey
+    })
+    .then(_ => right<Error, ContainerResponse>(_))
+    .catch(err => left<Error, ContainerResponse>(new Error(err)));
 }
 
-const servicesCollectionUrl = documentDbUtils.getCollectionUri(
-  documentDbDatabaseUrl,
-  SERVICE_COLLECTION_NAME
-);
-const serviceModel = new ServiceModel(documentClient, servicesCollectionUrl);
+const serviceContainer = cosmosClient
+  .database(cosmosDbName)
+  .container(SERVICE_COLLECTION_NAME);
+const serviceModel = new ServiceModel(serviceContainer);
 
 const aService: Service = Service.decode({
   authorizedCIDRs: [],
@@ -82,11 +60,10 @@ const aService: Service = Service.decode({
   throw new Error("Cannot decode service payload.");
 });
 
-const profilesCollectionUrl = documentDbUtils.getCollectionUri(
-  documentDbDatabaseUrl,
-  PROFILE_COLLECTION_NAME
-);
-const profileModel = new ProfileModel(documentClient, profilesCollectionUrl);
+const profileContainer = cosmosClient
+  .database(cosmosDbName)
+  .container(PROFILE_COLLECTION_NAME);
+const profileModel = new ProfileModel(profileContainer);
 
 const aProfile: Profile = Profile.decode({
   acceptedTosVersion: 1,
@@ -108,10 +85,10 @@ createDatabase(cosmosDbName)
   .then(() => createCollection("profiles", "fiscalCode"))
   .then(() => createCollection("sender-services", "recipientFiscalCode"))
   .then(() => createCollection("services", "serviceId"))
-  .then(() => serviceModel.create(aService, aService.serviceId))
+  .then(() => serviceModel.create({ ...aService, kind: "INewService" }).run())
   // tslint:disable-next-line: no-console
   .then(p => console.log(p.value))
-  .then(() => profileModel.create(aProfile, aProfile.fiscalCode))
+  .then(() => profileModel.create({ ...aProfile, kind: "INewProfile" }).run())
   // tslint:disable-next-line: no-console
   .then(s => console.log(s.value))
   // tslint:disable-next-line: no-console
